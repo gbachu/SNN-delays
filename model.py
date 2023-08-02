86b15ba7b29ae4dbcc32326ffbae50da8361ebd6
gbachu <140648390+gbachu@users.noreply.github.com>
2023-08-01 20:08:40 -0500

    Update model.py (pass 'Quadratic' type in build_model())

--- model.py
+++ model.py
@@ -16,7 +16,7 @@ class Model(nn.Module):
 
         self.config = config
 
-        self.build_model()
+        self.build_model('Quadratic')
         self.init_model()
 
         self.init_pos = []
